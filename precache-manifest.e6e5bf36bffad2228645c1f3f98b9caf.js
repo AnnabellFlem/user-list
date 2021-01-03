e066add62bb17ec2e3e1893232a8d24525017590
anna.tsukanova <anna.tsukanova@kyivstar.net>
2021-01-03 23:01:37 +0200

    Updates

---
+++ precache-manifest.e6e5bf36bffad2228645c1f3f98b9caf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4cd8193f9eb39b0dc8ac7a151ad796",
+    "url": "/user-list/index.html"
+  },
+  {
+    "revision": "54f9f83b09b771c43e06",
+    "url": "/user-list/static/css/main.8b542650.chunk.css"
+  },
+  {
+    "revision": "74bb638d410c5920a6fc",
+    "url": "/user-list/static/js/2.6baa0d7f.chunk.js"
+  },
+  {
+    "revision": "3c249b61ddfee6ef968b2fced6efcde9",
+    "url": "/user-list/static/js/2.6baa0d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f9f83b09b771c43e06",
+    "url": "/user-list/static/js/main.3e66ca82.chunk.js"
+  },
+  {
+    "revision": "1b8fbefef0f1b567d8af",
+    "url": "/user-list/static/js/runtime-main.f5cc254b.js"
+  }
+]);
