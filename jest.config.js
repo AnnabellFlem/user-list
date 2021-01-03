2caa103444207e044f324e935cf04e6d85ed95e7
anna.tsukanova <anna.tsukanova@kyivstar.net>
2021-01-03 15:33:18 +0200

    updated firebase config, jest config, styles for the main form

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,20 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom-fourteen',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
   moduleNameMapper: {
-    "\\.(css|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/src/__mocks__/assetsTransformer.js",
+    '\\.(css|sass|scss)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/assetsTransformer.js',
     '\\.svg': '<rootDir>/src/__mocks__/svgrMock.js',
-    "^components(.*)$": "<rootDir>/components$1",
-    "^assets(.*)$": "<rootDir>/assets$1",
-    "^config(.*)$": "<rootDir>/config$1",
+    '^components(.*)$': '<rootDir>/components$1',
+    '^assets(.*)$': '<rootDir>/assets$1',
+    '^config(.*)$': '<rootDir>/config$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/setupTests.ts',
+  ],
   transform: {
     '\\.(ts|js)x?$': 'ts-jest',
   },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
-  ],
-  moduleDirectories: ['node_modules', 'src']
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$'],
+  moduleDirectories: ['node_modules', 'src'],
 }
--- src/Services/firebase.ts
+++ src/Services/firebase.ts
@@ -4,13 +4,15 @@ import 'firebase/database'
 const config = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DB,
+  databaseURL:
+    'https://user-list-c0863-default-rtdb.europe-west1.firebasedatabase.app/',
   projectId: process.env.REACT_APP_PID,
   storageBucket: process.env.REACT_APP_SB,
   messagingSenderId: process.env.REACT_APP_MSID,
   appId: process.env.REACT_APP_APPID,
   measurementId: process.env.REACT_APP_MID,
 }
+
 firebase.initializeApp(config)
 const databaseRef = firebase.database().ref()
 
